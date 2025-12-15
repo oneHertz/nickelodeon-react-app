b893e6331f08a491085d119fa98c37edfd5c5f82
Raphael Stefanini <raphael@rphlo.com>
2025-12-15 13:54:54 +0200

    Improv

--- src/components/controls.jsx
+++ src/components/controls.jsx
@@ -12,13 +12,16 @@ function Controls(props) {
     } = props;
 
     return (
-        <div style={{margin: "0 15px 15px 15px"}}>
-        { isPaused ? <button onClick={onPlay}><i className="fa-solid fa-play fa-fw"></i></button> : <button onClick={onPause}><i className="fa-solid fa-pause fa-fw"></i></button>}
-        &nbsp;<button onClick={onNext}><i className="fa-solid fa-forward"></i></button>
-        &nbsp;<button onClick={onDownload}><i className="fa-solid fa-download"></i></button>
-        &nbsp;<button onClick={onSearch}><i className="fa-solid fa-magnifying-glass"></i></button>
-        &nbsp;<button onClick={onShowQueue}><i className="fa-solid fa-list"></i></button>
-        &nbsp;<button onClick={onUpload}><i className="fa-solid fa-cloud-arrow-up"></i></button>
+        <div className="controls" style={{margin: "0 15px 15px 15px"}}>
+        { isPaused ?
+          <button type="button" onClick={onPlay}><i className="fa-solid fa-play fa-fw"></i></button> :
+          <button onClick={onPause}><i className="fa-solid fa-pause fa-fw"></i></button>
+        }
+        &nbsp;<button type="button" onClick={onNext}><i className="fa-solid fa-forward"></i></button>
+        &nbsp;<button type="button" onClick={onDownload}><i className="fa-solid fa-download"></i></button>
+        &nbsp;<button type="button" onClick={onSearch}><i className="fa-solid fa-magnifying-glass"></i></button>
+        &nbsp;<button type="button" onClick={onShowQueue}><i className="fa-solid fa-list"></i></button>
+        &nbsp;<button type="button" onClick={onUpload}><i className="fa-solid fa-cloud-arrow-up"></i></button>
         </div>
     );
 }

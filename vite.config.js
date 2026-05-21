f87522fb10b6285627dc65d481ff72d6de0b8e8a
Raphael Stefanini <raphael@rphlo.com>
2026-05-21 08:45:35 +0300

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,57 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import {VitePWA} from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   build: {
     outDir: 'build',
+    
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2,gif,webm,mp4}'],
+        maximumFileSizeToCacheInBytes: 1e7,
+      },
+      manifest: {
+        "name": "Humppakone",
+        "short_name": "Humppakone",
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff",
+        "start_url": "/",
+        "display": "standalone"
+      }
+    })
+  ],
   define: {
-    // here is the main update
     global: 'globalThis',
   },
 })
